7d8b87cea564d27b97ae10077c867b0130f4a830
SoTosorrow <1064005438@qq.com>
2020-11-30 21:10:40 +0800

    调整

---
+++ src/rely/start.js
@@ -0,0 +1,33 @@
+var canvas, ctx, isContinue, timeoutID;
+
+function start(canvasName, func) {
+    if (timeoutID)
+        stop();
+
+    canvas = document.getElementById(canvasName);
+    ctx = canvas.getContext("2d");
+    isContinue = true;
+
+    var loop = function() {
+        func();
+        if (isContinue)
+            timeoutID = setTimeout(loop, 10);
+    }
+
+    loop();
+}
+
+function stop() {
+    clearTimeout(timeoutID);
+    isContinue = false;
+}
+
+function clearCanvas() {
+    if (ctx != null)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function start2(str){
+    cxt.fillStyle=str;
+    cxt.fillRect(0,0,150,75);
+    
+}
